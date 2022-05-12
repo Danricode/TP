3f5acf53df1ba7009dfaad9951ebc6237eff0a78
Daniel Silva <danielaudrasilva@gmail.com>
2022-05-12 12:13:08 +0200

    JSON manipulation on React Native

---
+++ TPJson/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
+import tp from './tp.json';
+
+class DemoApp extends Component {
+  render() {
+    return (
+      <View>
+
+        <FlatList
+          data={tp}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) =>
+
+            <View style={styles.container}>
+
+              <View style={styles.titleDescription}>
+
+                <Text style={styles.description}>{item.desc}</Text>
+
+                <Text style={styles.title}>{item.title}</Text>
+
+              </View>
+
+              <Image
+
+                style={styles.image}
+                source={{ uri: item.img }} />
+
+            </View>
+          }
+          keyExtractor={(item, index) => index.toString()}
+        />
+
+      </View>
+
+    );
+  }
+}
+
+export default DemoApp;
+
+const styles = StyleSheet.create({
+
+  container: {
+
+    padding: 15,
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "row-reverse",
+
+  },
+
+  titleDescription: {
+
+    flex: 1,
+    flexDirection: 'column-reverse',
+    flex: 1,
+    marginLeft: 10,
+
+  },
+
+  title:
+  {
+    fontWeight: "bold",
+  },
+
+  description: {
+
+    fontSize: 10,
+
+  },
+
+  image: {
+
+    flex: 1,
+    width: 50,
+    height: 200,
+  }
+});
